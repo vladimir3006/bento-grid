f5b8e2e09b55877d320e73f9f520cca2fb8664c3
Vladimir Valladares <vladimirvalladares@iMac-de-Vladimir.local>
2025-04-27 10:57:05 -0500

    resto del código del proyecto

---
+++ astro.config.mjs
@@ -0,0 +1,13 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
+
+// https://astro.build/config
+export default defineConfig({
+    vite: {
+        plugins: [tailwindcss()],
+      },
+
+      site: "https://github.com/vladimir3006",
+      base: "/bento-grid/",
+});
